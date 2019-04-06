95121422ecc87af05916b96e305d299a630bad17
mar bocatcat <mar.bocatcat@gmail.com>
2019-04-06 18:20:31 -0500

    changed software architecture to oop

--- pearch/pearch.py
+++ pearch/pearch.py
@@ -1,60 +1,81 @@
-#!bin/env python3
+#/bin/env python3
+
 import argparse
-from pathlib import Path
-
+import pathlib
+import fileinput
 
 '''
-    NAME:   pearch
-    USAGE:  pearch --file file.txt
-    DESCRIPTION:  a combination of the grep/cat UNIX command
+    Name: pearch
+    Usage: pearch --file some_file --pattern some_pattern --replace
+    Description: a tool similar to the popular UNIX command "grep/cat"
+    
+    Main Features:
+        1. Print the file input as default behavior.
+        2. with "--pattern" print the line with the pattern provided.
+        3. with "--replace", "--pattern" is needed.
+
 '''
 
 
-# TODO: ADD A try:except
-# TODO: ADD line numbers as a argument option.
-# TODO: ADD highlight color to result.
-# TODO: Search and replace feature.
-    #TODO: ADD write feature to replace.
+class Pearch:
+    
+    def __init__(self, file):
+        self.file = file
+    
+    def print_file(self):
+        return_file = str(self.file)
+        return f"File Path: {return_file}"
+    
+    def printer(self):
+        with open(self.file, "r") as reader:
+            result = [line.strip() for line  in reader.readlines()]
+            
+            for i, lines in enumerate(result):
+                print(lines)
+                
+    def print_pattern(self, target):
+        with open(self.file, "r") as reader:
+            result = [line for line in reader.readlines() if target in line]
+            print(result)
+            
+    def print_replace(self, target, pattern):
+        with fileinput.FileInput(self.file, inplace=True, backup=".bak") as file:
+            for line in file:
+                print(line.replace(target, pattern), end="")
 
 
-# argparse settings
-def parsed():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file', help='input file to be pearched',required=True)
-    parser.add_argument('--pattern', help='search for a specific pattern')
-    parser.add_argument('--l', help='add line numbers to result', action='store_true' )
-    parser.add_argument('--replace', help='replace pattern')
-    args = parser.parse_args()
-
-    # These will become a tuple to be unpacked later in the main loop
-    return args.file, args.pattern, args.l, args.replace
-
-
-# main logic loop
-def pearch(file, pattern , list_num, replace):
-
-    with open(file, 'r') as reader:
-        if bool(pattern):   # --pattern
-            if bool(replace):   # --replace
-                # NEED TO WORK ON WRITE LOGIC BELOW!
-                result = [reader.write(line.replace(pattern, replace)) for line in reader.readlines() if pattern in line]
-                print(result)
-            else:
-                # if pattern is in the file print the pattern(list)
-                result = [line for line in reader.readlines() if pattern in line]
-                print(result)
-        if list_num == True:    # --l
-            for i, line in enumerate(reader): print(f'{i}: {line}', end="")
-        else:
-            # print line with no options
-            for line in reader: print(line, end="")
 
 
 def main():
-    # unpack parsed tuples to the variable and give to pearch function
-    file, pattern, list_num, replace = parsed()
-    pearch(file, pattern, list_num, replace)
+    
+    def parse_args():
+        
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--file", required=True)
+        parser.add_argument("--target")
+        parser.add_argument("--pattern")
+        args = parser.parse_args()
 
+        return args.file, args.target, args.pattern
+        
+    
+    
+    def switch(file, target, pattern):
+        
+        pearch = Pearch(file)
+        
+        if bool(target) and bool(pattern):
+            pearch.print_replace(target, pattern)
+        elif bool(target):
+            pearch.print_pattern(target)
+        else:
+            pearch.printer()
+    
+    
+    file, target, pattern = parse_args()
+    switch(file, target, pattern)
+    
 
-if __name__ == '__main__':
+    
+if __name__ == "__main__":
     main()
